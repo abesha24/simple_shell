b52f28304f1074635dfb0edbb55f2b64622a29d8
Henok-Solomon-T <noksol03@gmail.com>
2023-05-26 12:40:45 -0700

    debug path_link_lis

--- path_linked_list.c
+++ path_linked_list.c
@@ -1,87 +1,34 @@
 #include "shell.h"
+
 /**
- * _getenv - gets an environment variable
- * @name: environment variable to get
- * Return: pointer to environment variable or NULL if there is no match
+ * env_linked_list - creates a linked list from environmental variables
+ * @env: environmental variables
+ * Return: linked list
  */
-char *_getenv(const char *name)
+list_t *env_linked_list(char **env)
 {
-	char **environ_copy;
-	char *variable, *value, *path;
-	int compare;
-	unsigned int path_length, environ_length, length, i;
+	list_t *head;
+	int i = 0;
 
-	environ_length = 0;
-	while (environ[environ_length] != NULL)
-		environ_length++;
-	environ_copy = NULL;
-	environ_copy = copy_env(environ_copy, environ_length);
-
-	length = _strlen((char *)name);
-	i = 0;
-	while (environ_copy[i] != NULL)
+	head = NULL;
+	while (env[i] != NULL)
 	{
-		variable = environ_copy[i];
-		compare = _strncmp((char *)name, variable, length);
-		if (compare == 1)
-		{
-			value = strtok(variable, "=");
-			value = strtok(NULL, "\n ");
-			if (value == 0)
-			{
-				errors(4);
-				exit(EXIT_FAILURE);
-			}
-			path_length = _strlen(value);
-			path = malloc(sizeof(char) * path_length + 1);
-			if (path == NULL)
-			{
-				errors(3);
-				return (NULL);
-			}
-			path = _strcpy(path, value);
-			free_dp(environ_copy, environ_length);
-			return (path);
-		}
+		add_end_node(&head, env[i]);
 		i++;
 	}
-	return (NULL);
+	return (head);
 }
 
-/**
- * copy_env - copies environment variable
- * @environ_copy: pointer to copy of environment variable
- * @environ_length: length of environment variable
- * Return: double pointer to copy of environment variable
- */
-char **copy_env(char **environ_copy, unsigned int environ_length)
+int main(void)
 {
-	char *variable;
-	unsigned int variable_length;
-	unsigned int i;
+	list_t *head;
 
-	environ_copy = malloc(sizeof(char **) * (environ_length));
-	if (environ_copy == NULL)
-	{
-		errors(3);
-		return (NULL);
-	}
+	head = NULL;
+	char p[12] = "PATH";
+	char *path = p;
+	env_linked_list(**path);
+	print_list(head);
 
-	i = 0;
-	while (i < environ_length)
-	{
-		variable = environ[i];
-		variable_length = _strlen(variable);
-
-		environ_copy[i] = malloc(sizeof(char) * variable_length + 1);
-		if (environ_copy[i] == NULL)
-		{
-			errors(3);
-			return (NULL);
-		}
-		_strcpy(environ_copy[i], environ[i]);
-		i++;
-	}
-
-	return (environ_copy);
+	return (0);
+	
 }
